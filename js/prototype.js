1fc52e6ffcddb1c51c99d355572bd960c12e8b12
Roberto Carvajal <roberto.carvajal@gmail.com>
2016-08-18 03:25:32 -0300

    base

---
+++ js/prototype.js
@@ -0,0 +1,70 @@
+// initial prototype
+$(function () {
+  window.loadData = loadData;
+  function loadData(path, year) {
+    d3.json(path, function(idx, json) {
+      var data = [];
+      console.log(json.result);
+      if(year == undefined) {
+        data = json.result.map(function (d) {
+          return d.price;
+        });
+      } else {
+        $.each(json.result, function(idx, d) {
+          if(d.year == year)
+            data.push(d.price);
+        });
+      }
+      var formatCount = d3.format(",.0f");
+
+      var margin = {top: 10, right: 30, bottom: 30, left: 30},
+        width = 800 - margin.left - margin.right,
+        height = 600 - margin.top - margin.bottom;
+
+      data.splice(data.indexOf(d3.max(data)), 1);
+      data.splice(data.indexOf(d3.min(data)), 1);
+
+      var x = d3.scaleLinear()
+        .domain([d3.min(data), d3.max(data)])
+        .range([0, width]);
+
+      var bins = d3.histogram()
+        .domain(x.domain())
+        .thresholds(x.ticks(20))(data);
+
+      var y = d3.scaleLinear()
+        .domain([0, d3.max(bins, function(d) { return d.length; })])
+        .range([height, 0]);
+
+      var svg = d3.select("body").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+      var bar = svg.selectAll(".bar")
+        .data(bins)
+        .enter().append("g")
+        .attr("class", "bar")
+        .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; });
+
+      bar.append("rect")
+        .attr("x", 1)
+        .attr("width", x(bins[0].x1) - x(bins[0].x0) - 1)
+        .attr("height", function(d) { return height - y(d.length); });
+
+      bar.append("text")
+        .attr("dy", ".75em")
+        .attr("y", 6)
+        .attr("x", (x(bins[0].x1) - x(bins[0].x0)) / 2)
+        .attr("text-anchor", "middle")
+        .text(function(d) { return formatCount(d.length); });
+
+      svg.append("g")
+        .attr("class", "axis axis--x")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+    });
+  }
+  loadData('data/berlingo.json');
+});
